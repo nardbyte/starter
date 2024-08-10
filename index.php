ac61911aaa0a97d04d08b481fb8df70572d0523e
nardbyte <zenenxd@gmail.com>
2024-08-10 03:15:42 -0400

    Update fix

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 require 'vendor/autoload.php';
-require 'configs/config.php';
+require 'inc/config.php';
 
 $smarty = new Smarty\Smarty;
 
 $smarty->setTemplateDir(__DIR__ . '/templates/');
 $smarty->setCompileDir(__DIR__ . '/templates/cache/');
-$smarty->setConfigDir(__DIR__ . '/configs/');
+$smarty->setConfigDir(__DIR__ . '/inc/');
 $smarty->setCacheDir(__DIR__ . '/cache/');
 
 // Asignar variables
